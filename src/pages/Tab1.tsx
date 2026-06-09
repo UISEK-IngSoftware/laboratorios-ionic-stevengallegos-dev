3dcfa3974aca688eecd6c9ab74e822cc2599565b
Steven Gallegos <steven.gallegos@uisek.edu.ec>
2026-06-08 22:56:49 -0500

    Laboratorio 7: componentes GUI

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,57 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+
+import "./Tab1.css";
+import { pencilOutline, trashOutline } from "ionicons/icons";
+import RepoItem from "../components/RepoItem";
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Lista de Repositorios</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Lista de Repositorios</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+        <IonList>
+          <RepoItem
+            name="Repositorio 1"
+            description="Descripcion del repositorio"
+            language="JavaScript"
+            avatarUrl="https://img.icons8.com/color/1200/the-flash-head.jpg"
+          />
+          <RepoItem
+            name="Repositorio 2"
+            description="Descripcion del repositorio"
+            language="JavaScript"
+            avatarUrl="https://www.shutterstock.com/image-vector/jakarta-indonesia-april-30-2022-600nw-2151111897.jpg"
+          />
+          <RepoItem
+            name="Repositorio 3"
+            description="Descripcion del repositorio"
+            language="JavaScript"
+            avatarUrl="https://static.vecteezy.com/system/resources/thumbnails/047/915/763/small/superhero-style-illustration-isolated-on-white-background-vector.jpg"
+          />
+          <RepoItem
+            name="Repositorio 4"
+            description="Descripcion del repositorio"
+            language="JavaScript"
+            avatarUrl="https://i.pinimg.com/736x/a7/15/c5/a715c5cdb21803ad46c06215e1ecd660.jpg"
+          />
+        </IonList>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,22 +1,43 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Tab3.css";
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil de usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil de usuario</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <div className="card-container">
+          <IonCard className="card">
+            <img alt="avatarUrl" src="https://img.icons8.com/color/1200/wonder-woman.jpg" />
+            <IonCardHeader>
+              <IonCardTitle>Steven Gallegos</IonCardTitle>
+              <IonCardSubtitle>ztilo</IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <p>Desarrollador de aplicaciones moviles</p>
+            </IonCardContent>
+          </IonCard>
+        </div>
       </IonContent>
     </IonPage>
   );
